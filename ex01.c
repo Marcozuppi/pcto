8c0f6db5cee85eadc4c98282a0653b8f877958fa
Marcozuppi <155971384+Marcozuppi@users.noreply.github.com>
2024-01-08 15:11:28 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+
+int main () {
+    int intero=1;
+    int brigataGabarron=69;
+    printf("patric>sergioramos : %d %d\n", intero , brigataGabarron);
+
+    return 0;
+}
---
+++ ex02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int intero=77;
+float decimale=2.3;
+char simbolo='£';
+double decimaleLungo=3.12345678;
+short numero= 6;
+long migliaia=1234;
+
+printf("variabili : %c %f %f %c %d\n", intero , decimale , simbolo , decimaleLungo , numero , migliaia);
+
+return 0;
+}
